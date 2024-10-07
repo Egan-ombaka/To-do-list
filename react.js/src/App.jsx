ba010ab6734872b555c9110a0e10f14d17f11bab
Egan-ombaka <eganombaka97@gmail.com>
2024-10-07 10:09:45 +0300

    To Do List

--- react.js/src/App.jsx
+++ react.js/src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import TodoInput from "./components/Todoinput"
 import TodoList from "./components/TodoList"
 
 function App() {
-  let todos = [
+
+  const [todos, setTodos] = useState([
     'Go to the Gym',
     'Eat more fruits and vegetables',
     'Pick the kids from school'
-  ]
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
 
   return (
     <>
-      <TodoInput />
+      <TodoInput handleAddTodos={handleAddTodos}/>
       <TodoList todos={todos} />
     </>
   )
